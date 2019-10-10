f9fc203567925dee9fd2d40e76f7537866eb3e7a
waqasulislam <waqasulislam@hotmail.com>
2019-10-10 16:50:37 +0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using Telerik.Windows.Controls;
+using Telerik.Windows.Controls.Docking;
+
+namespace DifferentToolWindowStyles
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = new ViewModel();
+        }
+    }
+
+    public class ViewModel
+    {
+        public ViewModel()
+        {
+            this.DisplayDateCommand = new DelegateCommand(OnDisplayDateChanged);
+        }
+
+        private void OnDisplayDateChanged(object obj)
+        {
+            
+        }
+
+        private DateTime displayDate;
+
+        public DateTime DisplayDate
+        {
+            get { return displayDate; }
+            set { displayDate = value; }
+        }
+
+        public DelegateCommand DisplayDateCommand { get; private set; }
+    }
+}
